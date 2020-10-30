7510030e6677fcd9e8a3883651f2d083b4412807
PiotOw <powczarczyk@vp.pl>
2020-10-30 17:35:11 +0100

    apexcharts installed, balance component added, mock balance added

---
+++ src/app/domain/balance/models/chart-options.ts
@@ -0,0 +1,22 @@
+import {
+    ApexAxisChartSeries,
+    ApexChart,
+    ApexFill,
+    ApexMarkers,
+    ApexStroke,
+    ApexTitleSubtitle,
+    ApexTitleSubtitle, ApexTooltip,
+    ApexXAxis, ApexYAxis
+} from 'ng-apexcharts';
+
+export interface ChartOptions {
+    series: ApexAxisChartSeries;
+    chart: ApexChart;
+    title: ApexTitleSubtitle;
+    xaxis: ApexXAxis;
+    fill: ApexFill;
+    stroke: ApexStroke;
+    markers: ApexMarkers;
+    yaxis: ApexYAxis;
+    tooltip: ApexTooltip;
+}
