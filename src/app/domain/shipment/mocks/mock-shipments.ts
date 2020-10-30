9d2cac22c8ab531042884e4b2a026e99202cbde0
PiotOw <powczarczyk@vp.pl>
2020-10-30 15:29:51 +0100

    shippment-tracker added, mocks created

---
+++ src/app/domain/shipment/mocks/mock-shipments.ts
@@ -0,0 +1,173 @@
+import {Shipment} from '../models/shipment';
+
+export const SHIPMENTS: Shipment[] = [
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'DBAS-SBAD-AHSJ-AHDS',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: 251,
+        eta: '03/25/2021'
+    },
+    {
+        trackingNumber: 'XXXX-XXXX-XXXX-XXXX',
+        from: {
+            city: 'New York',
+            Address: 'Awesome Street 14/21',
+        },
+        to: {
+            city: 'Warsaw',
+            Address: 'Plac Politechniki 1'
+        },
+        cashOnDelivery: null,
+        eta: '05/30/2021'
+    }
+    ];
--- src/app/views/dashboard/components/shipment-tracker/shipment-tracker.component.ts
+++ src/app/views/dashboard/components/shipment-tracker/shipment-tracker.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SHIPMENTS} from '../../../../domain/shipment/mocks/mock-shipments';
 
 @Component({
-  selector: 'jsn-shipment-tracker',
-  templateUrl: './shipment-tracker.component.html',
-  styleUrls: ['./shipment-tracker.component.scss']
+    selector: 'jsn-shipment-tracker',
+    templateUrl: './shipment-tracker.component.html',
+    styleUrls: ['./shipment-tracker.component.scss']
 })
 export class ShipmentTrackerComponent implements OnInit {
 
-  constructor() { }
+    SHIPMENTS = SHIPMENTS;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
